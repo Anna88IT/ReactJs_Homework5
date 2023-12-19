1cf647529de4c11d26cd45cb052f72f049fa0467
Anna88IT <anna.khach1988@gmail.com>
2023-12-19 10:27:50 +0400

    Changes

--- src/BackgroundMode.jsx
+++ src/BackgroundMode.jsx
@@ -1,14 +1,10 @@
 import React, {useEffect, useRef, useState} from "react";
 
 export const BackgroundMode = () => {
-    const [backgroundMode, setBackgroundMode] = useState(JSON.parse(localStorage.getItem("mode")) ? JSON.parse(localStorage.getItem("mode"))[0].mode : "light");
+    const [backgroundMode, setBackgroundMode] = useState(localStorage.getItem("mode") ? localStorage.getItem("mode") : "light");
     const h1Ref = useRef();
     useEffect(() => {
-        localStorage.setItem("mode", JSON.stringify(
-            [{
-                mode: backgroundMode
-            }]
-        ))
+        localStorage.setItem("mode", "light")
         document.body.className = backgroundMode;
         h1Ref.current.className = backgroundMode + "h1"
     }, [backgroundMode]);
